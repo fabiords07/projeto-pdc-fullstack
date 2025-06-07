abf58bbe366812ba413bca47bdedb397b9e78f71
Fabio Rangel <fabiorangeldiasfrd@gmail.com>
2025-06-07 16:54:22 -0300

    feat: create the components

---
+++ frontend/src/app/components/planos-section/planos-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planos-section',
+  imports: [],
+  templateUrl: './planos-section.component.html',
+  styleUrl: './planos-section.component.css'
+})
+export class PlanosSectionComponent {
+
+}
