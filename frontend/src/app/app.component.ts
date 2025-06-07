cceb52cab6b874d2e76c826fc3ab207c7a75c75b
Fabio Rangel <fabiorangeldiasfrd@gmail.com>
2025-06-07 17:07:05 -0300

    feat: add Axios and Tailwind configuration

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from "./components/hero/hero.component";
+import { PlanosSectionComponent } from "./components/planos-section/planos-section.component";
+import { ContactSectionComponent } from "./components/contact-section/contact-section.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, PlanosSectionComponent, ContactSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'plano-odontologico';
 }
