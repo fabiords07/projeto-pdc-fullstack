72c5818f14bcf2506d11c56e057ed5119377a553
Fabio Rangel <fabiorangeldiasfrd@gmail.com>
2025-06-07 17:30:12 -0300

    feat: adds the header

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    // Bloqueia/libera scroll do body quando menu está aberto
+    document.body.style.overflow = this.menuOpen ? 'hidden' : 'auto';
+  }
 }
